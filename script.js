701ba1b70fd38884e97847ab94211de2681b0ce7
Ali Heydari <aliheydari.web@gmail.com>
2025-08-16 18:37:38 +0330

    Developed By Ali Heydari
    16 Aug 2025

---
+++ script.js
@@ -0,0 +1,13 @@
+function checkPass() {
+        const correctUsername = "admin";
+        const correctPassword = "12345";
+
+        const usernameInput = document.querySelector("._user").value;
+        const passwordInput = document.querySelector("._pass").value;
+
+        if (usernameInput === correctUsername && passwordInput === correctPassword) {
+            alert("Welcome!");
+        } else {
+            alert("Incorrect username or password!");
+        }
+    }
