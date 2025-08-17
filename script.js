1bcd5da4e8e239420f0c2ae852d9748ac6836f23
Ali Heydari <aliheydari.web@gmail.com>
2025-08-17 12:27:26 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-function checkPass() {
-        const correctUsername = "admin";
-        const correctPassword = "12345";
+  function checkPass() {
+    const correctUsername = "admin";
+    const correctPassword = "12345";
+    const usernameInput = document.getElementById("_user");
+    const passwordInput = document.getElementById("_pass");
 
-        const usernameInput = document.querySelector("._user").value;
-        const passwordInput = document.querySelector("._pass").value;
-
-        if (usernameInput === correctUsername && passwordInput === correctPassword) {
-            alert("Welcome!");
-        } else {
-            alert("Incorrect username or password!");
-        }
-
+    if (usernameInput.value === correctUsername && passwordInput.value === correctPassword) {
+      alert("Welcome!");
+      usernameInput.value = ""; 
+      passwordInput.value = ""; 
+    } else {
+      alert("Incorrect username or password!");
     }
-
-
-
-
-
-
-
-
-
-
+  }
